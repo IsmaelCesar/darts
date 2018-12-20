31bb2e96e59031cea72061ec6246a565829547c4
leamscesar@gmail.com <leamscesar@gmail.com>
2018-12-20 11:43:07 -0300

    Running network with some simple parameter for the first time

--- cnn/darts_for_wine/experiment_darts_wine.py
+++ cnn/darts_for_wine/experiment_darts_wine.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 import os
-
+import sys
 import cnn.utils as utils
 from darts_for_wine.winedataset import WinesDataset
 from model_search import Network
@@ -20,14 +20,14 @@ from architect import Architect
 
 parser = argparse.ArgumentParser("DARTS for wine classification")
 parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
-parser.add_argument('--batch_size', type=int, default=10, help='batch size')
+parser.add_argument('--batch_size', type=int, default=1, help='batch size')
 parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
 parser.add_argument('--learning_rate_min', type=float, default=0.001, help='min learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
 parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
 parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
-parser.add_argument('--epochs', type=int, default=50, help='num of training epochs')
+parser.add_argument('--epochs', type=int, default=10, help='num of training epochs')
 parser.add_argument('--init_channels', type=int, default=1, help='num of init channels')#the initial channels of the data is one
 parser.add_argument('--layers', type=int, default=8, help='total number of layers')
 parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
@@ -42,6 +42,14 @@ parser.add_argument('--unrolled', action='store_true', default=False, help='use 
 parser.add_argument('--arch_learning_rate', type=float, default=3e-4, help='learning rate for arch encoding')
 parser.add_argument('--arch_weight_decay', type=float, default=1e-3, help='weight decay for arch encoding')
 args = parser.parse_args()
+
+
+log_format = '%(asctime)s %(message)s'
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+    format=log_format, datefmt='%m/%d %I:%M:%S %p')
+fh = logging.FileHandler(os.path.join('log.txt'))#args.save ,
+fh.setFormatter(logging.Formatter(log_format))
+logging.getLogger().addHandler(fh)
 
 global CLASSES_WINE
 
@@ -99,11 +107,11 @@ def run_experiment_darts_wine():
         logging.info('train_acc %f', train_acc)
 
 
-    valid_acc, valid_obj = infer(valid_queue,model,criterion)
+    valid_acc, valid_obj = infer(valid_queue,model,criterion,CLASSES_WINE)
     logging.info('valid_acc %f', valid_acc)
 
     file_index = 1
-    utils.save(model,os.path.join(args.save,"wine_"+str(file_index)+".pt"))
+    utils.save(model,os.path.join("wine_classes_"+str(CLASSES_WINE)+".pt"))#args.save,
 
 """
 The train e infer procedure were addapted for the leave one out technique
@@ -157,7 +165,7 @@ def train(train_queue, model,criterion,optimizer,num_classes):
 
   return top1.avg, objs.avg
 
-def infer(valid_queue, model, criterion):
+def infer(valid_queue, model, criterion,num_classes):
   """
   :param valid_queue: Data loader that randomly picks the samples in the Dataset, as defined in the previous procedure
   :param model:      Model of the network
@@ -170,17 +178,20 @@ def infer(valid_queue, model, criterion):
   model.eval()
 
   for step, (input, target) in enumerate(valid_queue):
-    input = Variable(input, volatile=True).cuda()
-    target = Variable(target, volatile=True).cuda(async=True)
+    input = Variable(input, volatile=True)#.cuda()
+    target = Variable(target, volatile=True)#.cuda(async=True)
 
     logits = model(input)
-    loss = criterion(logits, target)
+    loss = criterion(logits, torch.LongTensor([target]))
 
-    prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+    prec1, prec5 = utils.accuracy(logits, torch.LongTensor([target]), topk=(1, num_classes-1))
     n = input.size(0)
-    objs.update(loss.data[0], n)
-    top1.update(prec1.data[0], n)
-    top5.update(prec5.data[0], n)
+    objs.update(loss.data, n)
+    top1.update(prec1.data, n)
+    top5.update(prec5.data, n)
+    # objs.update(loss.data[0], n)
+    # top1.update(prec1.data[0], n)
+    # top5.update(prec5.data[0], n)
 
     if step % args.report_freq == 0:
       logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
