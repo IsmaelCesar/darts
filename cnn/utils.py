f08367f8a0af208e4097985afe97915dab9147f4
IsmaelCesar <leamscesar@gmail.com>
2019-12-06 23:49:36 -0300

    better summing

--- cnn/utils.py
+++ cnn/utils.py
@@ -94,7 +94,7 @@ class PerclassAccuracyMeter(object):
         return perclass_acc
 
     def compute_perclass_accuracy_with_precision_recall(self, epoch, is_train=True):
-        perclass_acc = self.confusion_matrix.diag() / self.confusion_matrix.sum(1)
+        perclass_acc = self.confusion_matrix.diag() / self.confusion_matrix.sum()
         precision_recall = self.__compute_precision_recall_and_f1()
         # Adding the values to the csv_list
         if self.current_epoch < epoch:
